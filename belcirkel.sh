e0c38ca574e037f2fbfe340146f93163a035bd3d
DavidMenting <david@davidmenting.nl>
2015-07-02 14:38:10 +0100

    Remove DC offset option deleted

--- belcirkel.sh
+++ belcirkel.sh
@@ -7,4 +7,4 @@
 ./sound_scripts/Playback_to_Speakers.sh
 ./sound_scripts/Record_from_DMIC.sh
 
-( pocketsphinx_continuous -inmic yes -kws keyphrase.list -backtrace yes -adcdev default -rawlogdir ./logdir -logfn ./logdir/pocketsphinx.log -time yes -samprate 16000 -remove_dc yes 3>&1 1>&2- 2>&3- ) | python ./processkws.py
+( pocketsphinx_continuous -inmic yes -kws keyphrase.list -backtrace yes -adcdev default -rawlogdir ./logdir -logfn ./logdir/pocketsphinx.log -time yes -samprate 16000 3>&1 1>&2- 2>&3- ) | python ./processkws.py
